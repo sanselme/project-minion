4fb1306068f075e75e989fe611af2781f458c0f7
Schubert Anselme <schubert@anselm.es>
2022-08-25 06:56:25 -0400

    Add run.sh, to launch qemu with provided image (#3)
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- cmd/run.sh
+++ cmd/run.sh
@@ -1,5 +1,40 @@
-#!/bin/sh
+#!/bin/bash
+
+# Copyright © 2022 Schubert Anselme <schubert@anselm.es>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 set -eux
 
-# TODO: run.sh
+: "${DEVICE_SMP:=2}"
+: "${DEVICE_MEM_MB:=2048}"
+
+: "${DEVICE_FIRMWARE:=$PWD/firmware/opensbi-$1.bin}"
+: "${DEVICE_KERNEL:=$PWD/kernel/u-boot-$1}"
+
+# run
+"qemu-system-${1}" \
+  -machine "${DEVICE_BOARD:-virt}" \
+  -smp "${DEVICE_SMP}" \
+  -m "${DEVICE_MEM_MB}M" \
+  -bios "${DEVICE_FIRMWARE}" \
+  -kernel "${DEVICE_KERNEL}" \
+  -device virtio-rng \
+  -device virtio-blk-device,drive=hd0 \
+  -device virtio-net-device,netdev=net0 \
+  -drive id=hd0,file=${2} \
+  -netdev user,id=net0,hostfwd=tcp::10022-:22,hostfwd=tcp::10080-:80,hostfwd=tcp::10443-:443 \
+  -serial mon:stdio \
+  -serial /dev/ttys4 \
+  -nographic
