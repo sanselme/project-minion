a22d22edf6149a6354d370c01f1fb455357e5a8c
Schubert Anselme <schubert@anselm.es>
2022-08-25 03:26:34 -0400

    Update clone-repo.sh, cloning repo if missing
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- scripts/clone-repo.sh
+++ scripts/clone-repo.sh
@@ -3,8 +3,9 @@
 set -eux
 
 # clone repositories
-echo "Cloning repositories..."
-git clone https://github.com/bootlin/buildroot-toolchains.git build/buildroot
-git clone https://github.com/qemu/qemu build/qemu
-git clone https://github.com/torvalds/linux build/linux
-git clone https://git.busybox.net/busybox build/busybox
+echo "Cloning repositories, if missing..."
+
+[ ! -d build/buildroot ] && git clone https://github.com/bootlin/buildroot-toolchains.git build/buildroot
+[ ! -d build/qemu ] && git clone https://github.com/qemu/qemu build/qemu
+[ ! -d build/linux ] && git clone https://github.com/torvalds/linux build/linux
+[ ! -d build/busybox ] && git clone https://git.busybox.net/busybox build/busybox
