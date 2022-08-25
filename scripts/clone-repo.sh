970f63ab56482db60cfe91f88e989aaeabb571c2
Schubert Anselme <schubert@anselm.es>
2022-08-25 19:36:27 -0400

    Initial riscv toolchain
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- scripts/clone-repo.sh
+++ scripts/clone-repo.sh
@@ -20,7 +20,5 @@ set -eux
 # clone repositories
 echo "Cloning repositories, if missing..."
 
-[ ! -d build/buildroot ] && git clone https://github.com/bootlin/buildroot-toolchains.git build/buildroot
-[ ! -d build/qemu ] && git clone https://github.com/qemu/qemu build/qemu
 [ ! -d build/linux ] && git clone https://github.com/torvalds/linux build/linux
 [ ! -d build/busybox ] && git clone https://git.busybox.net/busybox build/busybox
